7d5f0e532bdda24eec110f83c83619f1d4a23241
mrmeku <mrmeku@gmail.com>
2018-11-07 20:34:35 +0000

    Format and lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ShellModule } from "projects/shell/src/public_api";
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShellModule } from 'projects/shell/src/public_api';
 
 @NgModule({
   declarations: [AppComponent],
