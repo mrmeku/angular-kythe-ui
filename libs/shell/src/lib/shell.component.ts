9598372b7ea45d164fe2e1c436c6797ab78edfb9
Andrew Z Allen <me@andrewzallen.com>
2018-11-11 17:06:51 +0000

    Render the filename in the toolbar (#27)

--- libs/shell/src/lib/shell.component.ts
+++ libs/shell/src/lib/shell.component.ts
@@ -1,19 +1,32 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavigationEnd, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map, startWith, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'angular-kythe-ui-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent {
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(map(result => result.matches));
 
-  constructor(private readonly breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private readonly breakpointObserver: BreakpointObserver,
+    private readonly router: Router,
+    private readonly titleService: Title
+  ) {}
 
-  ngOnInit() {}
+  readonly filePath$ = this.router.events.pipe(
+    filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+    map(e => {
+      const u = new URL(`http://foo/${e.url}`);
+      return u.pathname.substr(2);
+    }),
+    tap(u => this.titleService.setTitle(`Angular Kythe UI - ${u}`))
+  );
 }
