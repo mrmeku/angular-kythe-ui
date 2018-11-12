10116865c200e53a81a263f0ae37ee926e2f830e
mrmeku <mrmeku@gmail.com>
2018-11-12 20:50:44 +0000

    Fix syntax highlighting. Switch to dark theme (#31)

--- apps/kythe-ui/src/app.module.ts
+++ apps/kythe-ui/src/app.module.ts
@@ -1,21 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { ShellComponent, ShellModule } from '@angular-kythe-ui/shell';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ShellModule } from '@angular-kythe-ui/shell';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  exports: [AppComponent],
-  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ShellModule,
     RouterModule.forRoot([])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ShellComponent]
 })
 export class AppModule {}
